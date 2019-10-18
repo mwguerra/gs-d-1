a1cf4680c63ea985c7a212c28cf89ec540d352db
Marcelo Wanderley Guerra <wguerra@globo.com>
2019-10-18 16:48:19 -0300

    Controle separado sobre tasks

--- indexMelhorado.js
+++ indexMelhorado.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const server = express()
 server.use(express.json())
-//gsdfg
 
 let projects = [{ 
     "id": "01234", 
@@ -13,6 +12,43 @@ let projects = [{
 }]
 
 ////////////////////////////
+// Prototypes
+////////////////////////////
+
+Array.prototype.findById = function(id) {
+    return this.findIndex(p => p.id == id)
+}
+
+Array.prototype.getById = function(id) {
+    return this.find(p => p.id == id)
+}
+
+////////////////////////////
+// Middlewares
+////////////////////////////
+
+server.use((req, res, next) => {
+    requestsCounter++
+    console.log(`Requisições: ${requestsCounter}`)
+})
+
+function verifyIfProjectExists (req, res, next) {
+    const { id } = req.params
+    const projectIndex = projects.findById(id)
+    
+    // Verifica se o projeto existe
+    if (projectIndex<0) {
+        return res.status(400).json({ "error": "The project doesn't exist." })
+    }
+    
+    // Cria novos elementos no req
+    req.projectIndex = projectIndex
+    req.project = projects.getById(id)
+
+    return next()
+}
+
+////////////////////////////
 // projects
 ////////////////////////////
 
@@ -20,35 +56,30 @@ server.get('/projects', (req, res) => {
     return res.json(projects)
 })
 
-server.get('/projects/:id', (req, res) => {
-    return res.json(projects.find(obj => obj.id == req.params.id))
+server.get('/projects/:id', verifyIfProjectExists, (req, res) => {
+    return res.json(req.project)
 })
 
 server.post('/projects', (req, res) => {
     const { id, title, tasks } = req.body
     const newProject = { id, title, tasks }
+
     projects.push(newProject)
-    return res.json(projects)
-})
-
-server.put('/projects/:id', (req, res) => {
-    const { id, title } = req.body
-
-    projects = projects.map(project => {
-        if (project.id == req.params.id) {
-            project.id = id
-            project.title = title
-        }
-        return project
-    })
     
     return res.json(projects)
 })
 
-server.delete('/projects/:id', (req, res) => {
-    // https://www.samanthaming.com/tidbits/35-es6-way-to-clone-an-array
-    const filteredProjects = projects.filter(project => project.id != req.params.id)
-    projects = [...filteredProjects]
+server.put('/projects/:id', verifyIfProjectExists, (req, res) => {
+    const { id, title } = req.body
+
+    req.project.title = title
+    
+    return res.json(req.project)
+})
+
+server.delete('/projects/:id', verifyIfProjectExists, (req, res) => {
+    projects.splice(req.projectIndex, 1)
+    
     return res.json(projects)
 })
 
@@ -56,19 +87,26 @@ server.delete('/projects/:id', (req, res) => {
 // tasks
 ////////////////////////////
 
-server.get('/projects/:id/tasks', (req, res) => {
-    return res.json(projects.find(obj => obj.id == req.params.id).tasks)
+server.get('/projects/:id/tasks', verifyIfProjectExists, (req, res) => {
+    return res.json(req.project.tasks)
 })
 
-server.post('/projects/:id/tasks', (req, res) => {
-    const { tasks } = req.body
-    projects = projects.map(project => {
-        if (project.id == req.params.id) {
-            project.tasks = tasks
-        }
-        return project
-    })
-    return res.json(projects)
+server.post('/projects/:id/tasks', verifyIfProjectExists, (req, res) => {
+    const { title } = req.body
+
+    req.project.tasks.push(title)
+
+    return res.json(req.project)
+})
+
+server.delete('/projects/:id/tasks', verifyIfProjectExists, (req, res) => {
+    const { task } = req.body
+    const { tasks } = req.project
+    const taskIndex = tasks.findIndex(t => t == task)
+
+    task.splice(taskIndex, 1)
+
+    return res.json(req.project)
 })
 
 ////////////////////////////
